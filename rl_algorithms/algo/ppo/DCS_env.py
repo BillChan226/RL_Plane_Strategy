3a998bc68e8384011ff5dc6f8c15db5354b3f80a
BillChan226 <czriphone@163.com>
2021-10-02 14:37:27 +0800

    more detailed

--- rl_algorithms/algo/ppo/DCS_env.py
+++ rl_algorithms/algo/ppo/DCS_env.py
@@ -17,25 +17,36 @@ class DCS_env:
         self.conn, self.addr = s.accept()
 
     def reset(self):
-        return
+        self.conn.sendall('ready'.encode('gb2312'))
+        print('successfully send reset command to client')
+        next_obserstr = self.conn.recv(self.BufferSize)
+        initial_obs = self.obs_parser(next_obserstr)
+        return initial_obs
 
     def step(self, a):
         #a = [1,2,3,4,5,6,7]
         action = '1' + '@' + str(a[0]) + ',' + str(a[1]) + ',' + str(a[2]) + ',' + str(a[3]) + ',' + str(a[3]) + ',' + str(a[4]) + ',' + str(a[5]) + ',' + str(a[6])
         #print(action)
-
-        # 读取客户端发送过来的数据
-        #data = self.conn.recv(self.BufferSize)
-        #print("Received from %s: %s" % (self.addr, data.decode('gb2312')))
-
         # 把客服端发送过来的数据又转发回去
         self.conn.sendall(action.encode('gb2312'))
         print('successfully send action to client')
         next_obserstr = self.conn.recv(self.BufferSize)
-        OurPlane, FriendPlane,Enemy1, Enemy2 = [], [], [], []
-        OurPlane =
+        print("Received from %s: %s" % (self.addr, data.decode('gb2312')))
+        full_next_obs, next_ourplane = self.obs_parser(next_obserstr)
+        rwd = self.reward(next_obserstr)
+        done = not()
         # 关闭客户端连接
         # conn.colse()
-    def reward(self):
-        rwd = -np.linalg.norm(self.getGroundTruth() - self.getTargetPos())
+        return full_next_obs, rwd, done, 0
+    def reward(self, obsstr):
+        ourplane, allyplane, enemyplane1, enemyplane2 = self.obs_parser(obsstr)
+        rwd = -min(np.linalg.norm(ourplane - enemyplane1), np.linalg.norm(ourplane - enemyplane2))
+        return rwd
+
     def obs_parser(self, obsstr):
+        ourplane, allyplane, enemyplane1, enemyplane2 =
+        observation = ourplane
+        observation = observation.extend(allyplane)
+        observation = observation.extend(enemyplane1)
+        observation = observation.extend(enemyplane2)
+        return observation, ourplane, allyplane, enemyplane1, enemyplane2
